37c7dd8f2f1417ef608799204ab03d2987322f5d
Shawn Sudaria <shawnlovecode14@gmail.com>
2023-11-22 20:12:14 +0800

    Updated

--- main.py
+++ main.py
@@ -10,16 +10,26 @@ import customtkinter
 # Add a table input to pass in those requests
 
 class ScatterLineChart(customtkinter.CTkFrame):
-    pass
+    def __init__(self, master):
+        super().__init__(master)
 
 #### The widgets in the frames must be in a grid
 class RequestTable(customtkinter.CTkFrame):
-    def __init__(self):
-       pass 
+    def __init__(self, master):
+        super().__init__(master)
 
+    
 class OptionMenu(customtkinter.CTkFrame):
-    def __init__(self):
-        self.inner_Disk = customtkinter.CTkEntry(self, placeholder_text="What is the highest number of the Disk")
+    def __init__(self, master):
+        super().__init__(master)
+
+        ### Entry for the highest DISK ###        
+        label_innerDisk = customtkinter.CTkLabel(self, text="What is the highest number of DISK")
+        label_innerDisk.grid(row=0, column=0)
+        self.inner_Disk = customtkinter.CTkEntry(self, placeholder_text="Input here")
+        self.inner_Disk.grid(row=1, column=0)
+
+        ### Entry box for the start head disk ###
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -27,6 +37,8 @@ class App(customtkinter.CTk):
         self.title("Disk Scheduling Algorithm")
         self.geometry("900x600")
 
+        self.optionMenu = OptionMenu(self)
+        self.optionMenu.pack()
 
 
 if __name__ == "__main__":
